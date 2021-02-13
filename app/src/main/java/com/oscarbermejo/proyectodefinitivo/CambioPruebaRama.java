549effda9da0008c755ac7b2616977de066a1b44
OscarBermejoDominguez <78918588+OscarBermejoDominguez@users.noreply.github.com>
2021-02-13 21:09:36 +0100

    CAmbios varioos
    nuevos cambios

--- app/src/main/java/com/oscarbermejo/proyectodefinitivo/CambioPruebaRama.java
+++ app/src/main/java/com/oscarbermejo/proyectodefinitivo/CambioPruebaRama.java
@@ -3,3 +3,5 @@ package com.oscarbermejo.proyectodefinitivo;
 public class CambioPruebaRama {
     //Esta clase es una prueba de como se cambian los datos en la rama
 }
+
+Cambios en la nueva rama prueba
